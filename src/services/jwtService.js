4068e637a52645f87ce583e4ea1b7f50528d1d12
raphael agostini <raphael.agostini@ynov.com>
2020-01-29 21:19:04 +0100

    login working, redirection and public pages set

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -13,17 +13,18 @@ module.exports = {
  */
 function verifyJwt(req, res, next){
     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-    if (token.startsWith('Bearer ')) {
-      // Remove Bearer from string
-      token = token.slice(7, token.length);
+    if(token !== undefined) {
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+        }
+        jwt.verify(token, 'mySuperSecrett', function (err, decoded) {
+            if (err) {
+                res.sendStatus(403)
+            } else {
+                req.decoded = decoded
+            }
+        })
     }
-    jwt.verify(token, 'mySuperSecrett', function(err, decoded){
-        if(err){
-            res.sendStatus(403)
-        }
-        else{
-            req.decoded = decoded
-        }
-    })
     next()
 }
