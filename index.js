821b78c96c6f8e1767725ac6a3b2038298df5170
Raphaël Agostini <raphael.agostini@ynov.com>
2020-02-13 11:34:51 +0100

    put back unified topology

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export const autoIncrement = require('mongoose-auto-increment');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://raphAdmin:kakashi%2313@raphcluster-hjbxp.mongodb.net/projetUf?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: false
+    useUnifiedTopology: true
 }, error => {
     if(error) {
         console.log(error);
